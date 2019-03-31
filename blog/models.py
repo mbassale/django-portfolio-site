7130a29440c8e3106122b772297aae444a03ac7a
Marco Bassaletti <bassaletti@gmail.com>
2019-03-31 00:33:15 -0300

    Created BlogPost model and registration with admin app.
    Signed-off-by: Marco Bassaletti <bassaletti@gmail.com>

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class BlogPost(models.Model):
+    title = models.CharField(max_length=255)
+    pub_date = models.DateTimeField()
+    body = models.TextField()
+    image = models.ImageField(upload_to='images/')
